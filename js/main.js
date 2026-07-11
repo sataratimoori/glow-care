f2c99b868c14504911ec11fa13645cbccc401193
sataratimoori <sataratimoori@gmail.com>
2026-07-11 16:47:37 +0430

    add navbar

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// HEADER SCROLL
+
+window.addEventListener("scroll", function () {
+    const header = document.getElementById("mainNav");
+    
+    if (window.scrollY > 100) {
+        header.classList.add("header-scrolled");
+    } else {
+        header.classList.remove("header-scrolled");
+    }
+});
