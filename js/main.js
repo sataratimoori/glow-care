3065be67b8aab14f57435f8328e7a5caf0ed46cf
Forough-Sahab <ahadyfuruqgmail.com>
2026-07-15 03:17:30 -0700

    forough

--- js/main.js
+++ js/main.js
@@ -23,3 +23,76 @@ const cards = document.querySelectorAll(".why-card");
 			});
 
 		});
+
+
+
+        const swiper = new Swiper(".carSwiper", {
+
+    loop:true,
+
+    autoplay:{
+        delay:3000,
+        disableOnInteraction:false,
+    },
+
+
+    effect:"coverflow",
+
+    coverflowEffect:{
+        rotate:30,
+        stretch:0,
+        depth:150,
+        modifier:1,
+        slideShadows:false,
+    },
+
+
+    slidesPerView:1,
+
+    spaceBetween:30,
+
+
+    pagination:{
+        el:".swiper-pagination",
+        clickable:true,
+    },
+
+
+    navigation:{
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev",
+    },
+
+
+    breakpoints:{
+
+        768:{
+            slidesPerView:2,
+        },
+
+
+        1200:{
+            slidesPerView:3,
+        }
+
+    }
+
+});
+
+
+
+
+
+
+document.querySelectorAll('.btn-add-cart').forEach(button => {
+  button.addEventListener('click', function() {
+    this.innerText = 'Added!';
+    this.style.backgroundColor = '#28a745'; // تغییر رنگ به سبز بعد از کلیک
+    
+    // بازگرداندن متن بعد از 2 ثانیه
+    setTimeout(() => {
+      this.innerText = 'Add to Cart';
+      this.style.backgroundColor = '#e91e63';
+    }, 2000);
+  });
+});
